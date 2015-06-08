35a11abc1c0a9948b582574764c9f121d216f94f
Pardo Walter <walterpardo7@gmail.com>
2015-06-08 01:31:19 -0700

    Implemented our tree and the ability to add family members

--- family-tree.js
+++ family-tree.js
@@ -25,3 +25,22 @@
 //                                     |                        
 //                                     +                        
 //                                   Nancy
+
+
+var Tree = function(data){
+	this.data     = data;
+	this.parent   = null;
+	this.children = [];
+};
+
+Tree.prototype.add = function(data){
+	var child    = new Tree(data);
+	child.parent = this;
+	this.children.push(child);
+};
+
+
+
+
+
+
