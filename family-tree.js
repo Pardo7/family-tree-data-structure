4977389ee27805824cfd545892a6d09524a772c6
Pardo Walter <walterpardo7@gmail.com>
2015-06-08 01:58:45 -0700

    Implemented our family tree

--- family-tree.js
+++ family-tree.js
@@ -39,6 +39,30 @@ Tree.prototype.add = function(data){
 	this.children.push(child);
 };
 
+var tree = new Tree("Nancy");
+tree.add("Adamn");
+tree.add("Jill");
+tree.add("Carl");
+
+var carl = tree.children[2];
+carl.add("Catherine");
+carl.add("Joseph");
+
+var jill = tree.children[1];
+jill.add("Kevin");
+
+var kevin = tree.children[1].children[0];
+kevin.add("Samuel");
+kevin.add("George");
+kevin.add("James");
+kevin.add("Aaron");
+
+var george = tree.children[1].children[0].children[1];
+george.add("Patrick");
+george.add("Robert");
+
+var james = tree.children[1].children[0].children[2];
+james.add("Mary");
 
 
 
